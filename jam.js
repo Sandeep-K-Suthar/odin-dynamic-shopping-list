f278cc82b46cbdc6f0cc9d497bcb89bb8d626518
Sandeep-K-Suthar <sandeep.khemani@outlook.com>
2025-03-01 22:49:56 +0500

    event handling

---
+++ jam.js
@@ -0,0 +1,34 @@
+const selectInput = document.querySelector('input');
+
+
+const selectClassList = document.querySelector(".list");
+const createUnorderedList = document.createElement('ul');
+selectClassList.appendChild(createUnorderedList);
+
+
+const selectAddButton = document.querySelector('.addBt');
+
+function addItem() {
+  const createListElement = document.createElement('li');
+  const listValues = selectInput.value.trim();
+
+  if (!listValues) return;
+
+  createListElement.innerText=listValues;
+  createUnorderedList.appendChild(createListElement);
+
+  const createDeleteButton = document.createElement('button');
+  createDeleteButton.style.marginLeft='1em';
+  createDeleteButton.innerText='Delete Me.';
+  createListElement.insertBefore(createDeleteButton,createListElement.nextSibling);
+
+  createDeleteButton.addEventListener('click',() => createListElement.remove());
+}
+
+selectAddButton.addEventListener('click', addItem);
+
+selectInput.addEventListener('keypress',(e)=>{
+  if (e.key ==='Enter'){
+    addItem();
+  }
+});
